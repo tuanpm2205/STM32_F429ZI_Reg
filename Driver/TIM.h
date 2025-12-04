2ad837e7833db99eb9948115eed0c333fa7dedc3
Phạm Minh Tuấn <phamminhtuan22072005sv@gmail.com>
2025-12-05 03:47:59 +0700

    TIM1 Typedef

--- Driver/TIM.h
+++ Driver/TIM.h
@@ -1,8 +1,37 @@
 #ifndef __TIM_H
 #define __TIM_H
 
+#include "TYPE.h"
+
+// Just develop for TIM1
+
 typedef struct{
+	__REG CR1;
+	__REG CR2;
+	__REG SMCR;
+	__REG DIER;
+	__REG SR;
+	__REG EGR;
+	__REG CCMR1;
+	__REG CCMR2;
+	__REG CCER;
+	__REG CNT;
+	__REG PSC;
+	__REG ARR;
+	__REG RCR;
+	__REG CCR1;
+	__REG CCR2;
+	__REG CCR3;
+	__REG CCR4;
+	__REG BDTR;
+	__REG DCR;
+	__REG DMAR;
+}TIM1_Typedef;
+
+#define TIM1_ADDRESS_BASE				0x40010000UL
+#define TIM1										(volatile TIM1_Typedef *)TIM1_ADDRESS_BASE
 	
-}TIM_Typedef;
+
+void TIM1_Init();
 
 #endif
