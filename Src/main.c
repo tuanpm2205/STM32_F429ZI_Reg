7516dea3e6612db0e12bfec184bbc996666cb3dd
Phạm Minh Tuấn <phamminhtuan22072005sv@gmail.com>
2025-12-06 03:35:11 +0700

    test function delay

--- Src/main.c
+++ Src/main.c
@@ -2,6 +2,7 @@
 #include <GPIO.h>
 #include <RCC.h>
 #include <EXTI.h>
+#include <TIM.h>
 
 void mDelay(volatile uint32_t time){
 	while(time--);
@@ -11,12 +12,17 @@ int main(){
 	RCC_Config_Default();
 	RCC_Enable_GPIOB();
 	RCC_Enable_GPIOC();
-	RCC->APB2ENR |= (1 << 14);
+	RCC_Enable_SYSCFG();
+	RCC_Enable_TIM1();
+	TIM1_Init();
 	GPIO_Config(GPIOB, PIN_14, MODE_OPP);
-	GPIO_Config(GPIOC, PIN_13, MODE_IPD);
-	EXTI13_Init();
+	GPIO_Config(GPIOB, PIN_7, MODE_OPP);
+	//EXTI13_Init();
+	
+	EXTI_Config(PIN_13, PORTC, MODE_RASING);
+	
 	while(1){
-		GPIO_Toggle_Pin(GPIOB, PIN_14);
-		mDelay(100000000);
+		GPIO_Toggle_Pin(GPIOB, PIN_7);
+		Delay_ms(1000);
 	}
 }
