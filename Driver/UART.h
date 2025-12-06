615e99773faa64fbcd37704c06123fecbdca40e9
Phạm Minh Tuấn <phamminhtuan22072005sv@gmail.com>
2025-12-06 14:11:13 +0700

    UART Typedef

---
+++ Driver/UART.h
@@ -0,0 +1,21 @@
+#ifndef __UART_H
+#define __UART_H
+
+#include "TYPE.h"
+
+typedef struct{
+	__REG SR;
+	__REG DR;
+	__REG BRR;
+	__REG CR1;
+	__REG CR2;
+	__REG CR3;
+	__REG GTPR;
+}UART_Typedef;
+
+#define UART1_ADDRESS_BASE			0x40011000UL
+#define UART1										((volatile UART_Typedef*)(UART1_ADDRESS_BASE))
+	
+void UART1_Init();
+
+#endif
